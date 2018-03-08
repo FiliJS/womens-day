118d84c428ac5fec5a8c3cae11dbe98d255cacff
Fili Santillan <fili06604@gmail.com>
2018-03-07 23:17:45 -0600

    🎉 Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const superPowersList = [
+    "Engineer",
+    "Teacher",
+    "Programmer",
+    "Architect",
+    "Mom",
+    "Psychologist",
+    "Dentist",
+    "Astronaut",
+    "Superheroine",
+    "Chingona",
+    "Veterinary",
+    "Lawyer",
+    "CEO",
+    "CTO",
+    "Entrepreneur",
+    "Politician"
+];
+
+const $image = document.createElement("img"),
+      $imageContainer = document.getElementById("image-container"),
+      $superPower = document.getElementById("superpower"),
+      imagePath = `./images/women-0${Math.round(Math.random() * (4 - 1)) + 1}.png`;
+
+$image.src = imagePath;
+$imageContainer.appendChild($image);
+$superPower.innerText = superPowersList[Math.round(Math.random() * (superPowersList.length - 1))];
